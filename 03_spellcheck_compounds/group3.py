f50adb30860d36e7ea645e6da7acd6735debd371
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2015-11-16 17:24:09 +0100

    Running baseline

---
+++ 03_spellcheck_compounds/group3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- encoding:utf8 -*-
+
+from automaton import compounds_automaton
+import fileinput
+import pickle
+
+wico = pickle.load(open('results_wico.p', 'r'))
+
+for line in fileinput.input():
+    line = line.strip()
+    words = line.split(' ')
+    result = []
+    for w in words:
+        # Detect if word is unknown
+        if w in wico:
+            result.append('{ORIG_ORTH="%s"}%s' % (w, wico[w]))
+        # Run levenshtein on LeFFF (loaded from where?)
+        else:
+            result.append(w)
+    print(' '.join(result))
