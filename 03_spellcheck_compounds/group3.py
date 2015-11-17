1825a64285c3defd434102414a2e296151643cd8
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2015-11-17 11:27:41 +0100

    Plugging amalgams into main script

--- 03_spellcheck_compounds/group3.py
+++ 03_spellcheck_compounds/group3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding:utf8 -*-
 
-from utils import closest_word
+from utils import closest_word, expand_amalgam
 from automaton import compounds_automaton
 import fileinput
 import pickle
@@ -14,24 +14,24 @@ for line in fileinput.input():
     words = line.split(' ')
     spellchecked = []
     # Step 1: spellcheck
-    for i in range(len(words)):
+    for w in words:
         # Detect if word is unknown
-        if words[i] in wico:
-            spellchecked.append('{ORIG_ORTH="%s"}%s' % (words[i], wico[words[i]]))
+        if w in wico:
+            spellchecked.append('{ORIG_ORTH="%s"}%s' % (w, wico[w]))
         else:
             lefff = []  # Haha. I know.
-            lefff_corr = closest_word(lefff, words[i])
+            lefff_corr = closest_word(lefff, w)
             if lefff_corr:
-                spellchecked.append('{ORIG_ORTH="%s"}%s' % (words[i], lefff_corr))
+                spellchecked.append('{ORIG_ORTH="%s"}%s' % (w, lefff_corr))
             else:
-                spellchecked.append(words[i])
+                spellchecked.append(w)
 
     # Step 2: compound words
     merged = []
     slen = len(spellchecked)
     i = 0
     while i < len(spellchecked):
-        res = compounds_automaton.recognize(words[i:])
+        res = compounds_automaton.recognize(spellchecked[i:])
         if res:
             comp, clist, tag, clen = res
             merged.append('{ORIG_SEG=[%s]}%s__%s' % (','.join(clist), comp, tag))
@@ -40,4 +40,12 @@ for line in fileinput.input():
             merged.append(spellchecked[i])
             i += 1
 
-    print(' '.join(merged))
+    # Step 3: amalgams
+    expanded = []
+    for w in merged:
+        am = expand_amalgam(w)
+        if len(am) > 1:
+            am = map(lambda x: '{AML="%s"}%s' % (w, x), am)
+        expanded.extend(am)
+
+    print(' '.join(expanded))
