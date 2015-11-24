e480f46a2dcffee91af64cdc7e128adc49127333
Arthur Lapraye <arthurlapraye@yahoo.fr>
2015-11-24 17:24:39 +0100

    script qui étiquette sur stdin

--- 02_morpho/morpho.py
+++ 02_morpho/morpho.py
@@ -5,15 +5,31 @@ import re
 import random
 from functools import partial
 from collections import defaultdict
-import pickle
+import cPickle as pickle
 import sys
+from optparse import OptionParser
 
-#Variables globales
+if __name__ == "__main__":
+	usage=""
+	p = OptionParser(usage=usage)
+	p.add_option("-i","--iteration", action="store",dest="iteration", default=8,
+	help=u"Nombres d'itérations du perceptron. Par défaut 8")
 
-TEST=9
+	p.add_option("-c", "--confusion",
+		              action="store_true", dest="matrice", default=False,
+		              help=u"")
+
+	(op,args)=p.parse_args()
+	#Variables globales
+
+	ITER=int(op.iteration)
+	MATRICE=op.matrice
+	TEST=9
+	LATEX=False
+	PERCENT=True
+
 CAPITALES=u'ABCDEFGHIJKLMNOPQRSTUVWXYZÉÈÇÀÔÖÎÂÏÛÊË'
-LATEX=False
-PERCENT=True
+CLASSESFERMEES= [u'ADJWH',u'ADVWH',u'CC',u'CLO',u'CLR',u'CLS',u'CS',u'DET',u'DETWH',u'P',u'PRO',u'PROREL',u'PROWH']
 
 #Décorateur pour la mise en cache dans un dictionnaire de résultats de l'appel à une fonction
 #Permet de limiter les appels à des fonctions couteuses en temps de calcul
@@ -50,26 +66,29 @@ def readlexicon(fichier):
 			else:
 				forme,cat=ligne
 			
-			#Mise en forme des valeurs récupérées :
-			#Suppression des espaces
-			forme=forme.strip()
-			forme=re.sub(r"[ ]+","_",forme,0,re.UNICODE)
-			#Suppression des blancs en fin de lignes 
 			cat=cat.strip()
 			
+			if cat not in CLASSESFERMEES:
+				#Mise en forme des valeurs récupérées :
+				#Suppression des espaces
+				forme=forme.strip()
+				forme=re.sub(r"[ ]+","_",forme,0,re.UNICODE)
+				#Suppression des blancs en fin de lignes 
+				
 			
-			cats.add(cat)
 			
-			#c est une variable aléatoire comprise entre 0 et TEST
-			#Pour chaque mot, si c est égal à la valeur TEST
-			#La paire (forme,cat) est ajoutée dans le corpus de test
-			#Sinon elle rejoint le corpus d'entraînement
-			if c == TEST:
-				test.append((forme,cat))
-			else:
-				train.append((forme,cat))
+				cats.add(cat)
 			
-			c=random.randint(0,TEST)
+				#c est une variable aléatoire comprise entre 0 et TEST
+				#Pour chaque mot, si c est égal à la valeur TEST
+				#La paire (forme,cat) est ajoutée dans le corpus de test
+				#Sinon elle rejoint le corpus d'entraînement
+				if c == TEST:
+					test.append((forme,cat))
+				else:
+					train.append((forme,cat))
+			
+				c=random.randint(0,TEST)
 			
 			
 	return test,train,cats
@@ -133,13 +152,18 @@ def score(w,traits):
 
 #Fonction de classification du perceptron
 def classify(poids,traits):
-	return max(poids.keys(),key=lambda x: score(poids[x], traits))
+	toto=poids.keys()
+	a=max(toto,key=lambda x: score(poids[x], traits))
+	toto.remove(a)
+	b=max(toto,key=lambda x: score(poids[x], traits))
+	return [a,b]
+	
 	#return sorted(poids.keys(),key=lambda x: score(poids[x], traits),reverse=True)
 
 #Fonction renvoyant un defaultdict ayant des floats pour valeur
 #Utilisée pour créer le vecteur de poids car Pickle n'aime pas les lambdas
 def defloat():
-	return defaultdict(float)
+	return defaultdict(float) 
 
 #Fonction d'entraînement du perceptron
 #Entraîne un perceptron sur un corpus pour un nombre maximal d'itérations fixées à l'avance
@@ -168,16 +192,21 @@ def perceptronmaker(cats,corpus,itermoi=10,averaged=True,shuffled=True,poids=def
 		for (e,c) in corpus:
 			truecat=c
 			exemple=getfeatures(e)
-			cat=classify(poids, exemple) #[0]
-			if cat != truecat:
+			cat,zcat=classify(poids, exemple)[:2]
+			#if cat != truecat:
+			if truecat not in [cat,zcat]:
 				allright=False
 				for trait in exemple:
 					poids[truecat][trait] = poids[truecat][trait] + exemple[trait]
 					poids[cat][trait] = poids[cat][trait] - exemple[trait]
 					
+					#poids[zcat][trait] = poids[zcat][trait] - exemple[trait]
+					
 					#Accumulateur, utilisé pour le moyennage du perceptron 
 					accum[truecat][trait] = accum[truecat][trait] + (exemple[trait]*i)
 					accum[cat][trait] = accum[cat][trait] - (exemple[trait]*i)
+					
+					#accum[zcat][trait] = accum[zcat][trait] - (exemple[trait]*i)
 				
 			i+= 1
 					
@@ -188,7 +217,6 @@ def perceptronmaker(cats,corpus,itermoi=10,averaged=True,shuffled=True,poids=def
 		for w in poids:
 			for feat in poids[w]:
 				poids[w][feat] = poids[w][feat] - (accum[w][feat]/(i+1))
-				#poids[w][feat] = (accum[w][feat]/(i+1))
 	
 	print "...fait"
 			
@@ -221,17 +249,21 @@ def testit(test,perceptron,matrice=True):
 	for (e,c) in test:
 		truecat=c
 		realcats[truecat] += 1
-		y=perceptron(getfeatures(e))
-		#y,z=perceptron(getfeatures(e))[:2]
+		#y=perceptron(getfeatures(e))
+		y,z=perceptron(getfeatures(e))[:2]
 		matcon[truecat,y] += 1
-		#matcon[truecat,z] += 1
+		matcon[truecat,z] += 0.5#1
 			
 		#Nice = total des éléments bien catégorisés
-		if y == truecat:
+		#if y == truecat:
+		if truecat in [y,z]:
 			nice += 1.0
+		#elif z == truecat:
+		#	nice += 0.5
 		else:
 			errs += 1.0
-		
+	
+	minimum=None
 	#M : option pour afficher la matrice de confusion
 	if matrice:
 		
@@ -241,7 +273,11 @@ def testit(test,perceptron,matrice=True):
 		print "Matrice de confusion :"
 		z=sorted(cats)
 		print sep +sep.join(z) + endline
+		
 		for ca in z:
+			if realcats[ca] != 0 and ((not minimum) or matcon[minimum] > matcon[ca,ca]):
+					minimum=(ca,ca)
+			
 			if realcats[ca] != 0.0:
 				if PERCENT:
 					p=[str((100*matcon[ca,a])/realcats[ca])[:4]+percentsign+sep for a in z]
@@ -249,11 +285,14 @@ def testit(test,perceptron,matrice=True):
 					p=[str(matcon[ca,a]) for a in z]
 					
 				print ca + sep + "".join(p) + endline
+		
 	
+		print u"Classe la plus mal reconnue :",minimum[0],"précision de :",(100.0*matcon[minimum]/realcats[minimum[0]]),percentsign
 	#Calcul de la précision globale : éléments bien catégorisés sur nombre total d'éléments
 	p=nice/len(test)
 	precision= str(100*p)+"%" if PERCENT else str(nice) + "/" + str(wc)
 	print u"Précision globale : " + precision  + "\n"
+	
 	return p
 
 #Fonction qui extrait et affiche les traits les plus informatifs du vecteur de poids du perceptron
@@ -266,13 +305,13 @@ def meilleurstraits(weight):
 		#print "Pire traits pour " + z
 		#print "\t"+"\n\t".join([ "\""+x+"\" : "+str(weight[z][x]) for x in feats[-10:] ]) + "\n"
 		
-
+#Fonction pour sauver le vecteur de poids dans un fichier
 def saveweights(weight):
 	with open("weights.pickle",'w') as toto:
 		pickle.dump(weight,toto)
+
 	
-	return 0
-	
+#Fonction pour charger un vecteur de poids à partir d'un fichier
 def importweights(filename):
 	with open(filename) as toto:
 		return pickle.load(toto)
@@ -284,31 +323,34 @@ def importweights(filename):
 #"../05_pcfg/lexicon2.txt"
 #"../../TP Parsing M2LI/lexique_cmpnd_TP_unicode.txt"
 
+if __name__ == "__main__":
+	test,train,cats=list(),list(),set()
+	for e in args:
+		x,y,z=readlexicon(e)
+		test += x
+		train += y
+		cats.update(z)
 
-test,train,cats=list(),list(),set()
-for e in sys.argv[1:]:
-	x,y,z=readlexicon(e)
-	test += x
-	train += y
-	cats.update(z)
+	print len(cats),"catégories : ",sorted(cats)
+	print "taille du corpus d'entraînement : ",len(train)," et du corpus de test : ",len(test)
+	print "Entraînement du perceptron :"
+	weight=perceptronmaker(cats,train,itermoi=ITER)
 
-print len(cats),"catégories : ",sorted(cats)
-print "taille du corpus d'entraînement : ",len(train)," et du corpus de test : ",len(test)
-print "Entraînement du perceptron :"
-weight=perceptronmaker(cats,train,itermoi=8)
+	perceptron=partial(classify,weight)
 
-perceptron=partial(classify,weight)
+	#precision=0.0
+	#for e in test:
+	#	if perceptron(getfeatures(e)) == test[e]:
+	#		precision += 1
+	#precision= (100*precision/len(test))
+	#print precision,"%"
 
-#precision=0.0
-#for e in test:
-#	if perceptron(getfeatures(e)) == test[e]:
-#		precision += 1
-#precision= (100*precision/len(test))
-#print precision,"%"
+	testit(test,perceptron,matrice=MATRICE)
 
-testit(test,perceptron,matrice=False)
+	#raw_input()
+	#meilleurstraits(weight)
+	saveweights(weight)
 
-#raw_input()
-#meilleurstraits(weight)
-#saveweights(weight)
+	#while True:
+	#	print perceptron(getfeatures(raw_input()))
 
