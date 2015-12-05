961bc330a91ad882303c56f4c790a0ce6a6043bb
muvikt <muvikt@gmail.com>
2015-12-06 00:49:05 +0100

    nettoyage du code

--- 04_namedEntity/recognitionNE.py
+++ 04_namedEntity/recognitionNE.py
@@ -3,14 +3,13 @@
 
 import sys
 import re
+import codecs
 
 class Texte():
   
   def __init__(self):
         self.stringOrig=""
         self.phrases=[]
-        
-        
   
   def readString(self,data):
     for string in data:
@@ -22,16 +21,9 @@ class Texte():
     output=""
     self.readString(string)
     for phrase in self.phrases:
-      #if phrase.words[0].token=="":
-	#return self.stringOrig
-      #phrase.printP()
       phrase.searchEN()
       output+=phrase.outputString()+'\n'
     return output
-    
-  
-    
-   
 
 class Phrase():
   
@@ -53,8 +45,8 @@ class Phrase():
       el.fromString(w)
       self.tokens.append(el.token)
       self.words.append(el)
+      print w
       self.string+=el.token+" "
-    #print self.string
   
   def checkSplit(self, split2check):#pour eviter le split sur des espaces entre les "{}"
     for el in split2check:
@@ -80,38 +72,34 @@ class Phrase():
 	  
   
   def detectCandidatesEN(self):#cherche les candidats potentiels pour les EN
-    #print "str", self.string
     candidatesEN=[]
-    #match=re.findall(r"((?:[A-Z](?:\.|\w*)\s(?:(?:de|du|le|von|van)\s)?(?:(?:[A-Z]+(?:[a-z]+)?)\s)))|([A-Z]\w*\s)|", self.string)
-    self.candidates=re.findall(r"((?:\b[A-Z]\w*)(?:\s|-)(?:[A-Z]\w*\s)*|(?:\b[A-Z]\w*\s))", self.string)
+    #self.candidates=re.findall(r"((?:\b[A-Z]\w*)(?:\s|-)(?:[A-Z]\w*\s)*|(?:\b[A-Z]\w*\s))", self.string)
+    self.candidates=re.findall(r"((?:\b[A-Z](?:\w*|\.))(?:\s|-)(?:(?:von|de|du|le|van)\s)?(?:[A-Z]\w*\s)*|(?:(?:\b[A-Z]\w*\s)))", self.string)
+
     for i in xrange(len(self.candidates)):
       self.candidates[i]=self.candidates[i][:-1]
-    #print "yeees", self.candidates
+    #print self.candidates
     
     
-  def searchEN(self):
+  def searchEN(self):#verfifie si les candidats sont présents dans nos dictionnaires
     #self.entities.loadDB("04_namedEntity/Pers.txt","04_namedEntity/Loc.txt", "04_namedEntity/Orgs.txt")
     self.entities.loadDB("04_namedEntity/Pers.txt","04_namedEntity/Pers_var.txt", "04_namedEntity/Loc.txt", "04_namedEntity/Orgs.txt")
     self.search_ruleBased_EN()
     self.detectCandidatesEN()
-    #print 'Cand', self.candidates
     for word in self.candidates:
-	#print 'word', word
 	if word in self.entities.names and word!="Les" and word!="Le":
-	  #print 'YEEEEH'
 	  properName=self.entities.names[word]
-	  #print word, properName
-	  self.fusion_des_tokens(word,'_PERS',self.entities.namesLink[word])
+	  self.fusion_des_tokens(word,'_PERS',self.entities.namesLink[properName])
 	if word in self.entities.places:
 	  self.fusion_des_tokens(word,'_LOC',self.entities.places[word])
 	if word in self.entities.orgs:
 	  self.fusion_des_tokens(word,'_ORG',self.entities.orgs[word]) 
   
-  def fusion_des_tokens(self, tokenAfusionner,etiquette,lien):
+  def fusion_des_tokens(self, tokenAfusionner,etiquette,lien): #actualise les données d'une EN detectée
     split=tokenAfusionner.split()
     if len(split)==1:
       index=self.tokens.index(tokenAfusionner)
-      if self.words[index].attribut=="":
+      if self.words[index].attribut=="":		
 	    self.words[index].attribut='{ORIG=\''+tokenAfusionner+'\';LIEN=\''+lien+'\';}'
       else:
 	    attributNouv=self.words[index].attribut[:-1]+'ORIG=\''+tokenAfusionner+'\';LIEN=\''+lien+'\';}'
@@ -135,36 +123,14 @@ class Phrase():
 	i+=1	
 	del(self.words[index+1])
       
-      #actualisation self.string
+      #actualisation de self.string
       self.string=""
       self.tokens=[]
       for w in self.words:
 	self.tokens.append(w.token)
 	self.string+=w.token+" "
   
-
-  #def searchEN(self):
-    #self.entities.loadDB("04_namedEntity/Pers.txt","04_namedEntity/Pers_var.txt", "04_namedEntity/Loc.txt", "04_namedEntity/Orgs.txt")
-    ##self.entities.loadDB("Pers.txt","Pers_var.txt", "Loc.txt", "Orgs.txt")
-    #self.search_ruleBased_EN()
-    #self.detectCandidatesEN()
-    #for word in self.words:
-      #if len(word.token)>0 and word.token[0].isupper():
-	#if word.token in self.entities.names:
-	  #properName=self.entities.names[word.token]
-	  #if word.attribut=="":
-	    #word.attribut='{ORIG=\''+word.token+'\';LIEN=\''+self.entities.namesLink[properName]+'\'}'
-	  #else:
-	    #word.attribut=word.attribut[:-1]+';ORIG=\''+word.token+'\';LIEN=\''+self.entities.namesLink[properName]+'\'}'
-	  #word.token='_PERS'
-	#if word.token in self.entities.places:
-	  #if word.attribut=="":
-	    #word.attribut='{ORIG=\''+word.token+'\';LIEN=\''+self.entities.places[word.token]+'\';}'
-	  #else:
-	    #word.attribut=word.attribut[:-1]+';ORIG=\''+word.token+'\';LIEN=\''+self.entities.places[word.token]+'\'}'
-	  #word.token='_LOC'
-  
-  def search_ruleBased_EN(self):
+  def search_ruleBased_EN(self):# detect les personnes commencant par M., Mme, Mlle
     if re.search(r"\b(M.|Mme|Mlle)\b", self.string):
       match=re.search(r"(M.|Mme|Mlle)\s?(\b[A-Z]\w+)\s?((?:de|von|le)?\s?[A-Z]\w+)?",self.string)
       ne = match.group(0)
@@ -218,8 +184,10 @@ class Word():
 	self.token=" "
 	self.attribut="{}"
       else:
-	self.token=match.group(2)
-	self.attribut=match.group(1)
+	token=match.group(2)
+	att=match.group(1)
+	print token, att
+	self.attribut=att
     else:
       self.token=string
       
@@ -240,17 +208,15 @@ class NameEntities():
 	  newLine=[]
 	  self.namesLink[line[0]]=line[1][:-1]
 	  line=file.readline()
-	#print self.names
 	file.close()
 	
 	file=open(dicPersVariants)
 	line=file.readline()
 	while line<>"":
 	  line=line.split('\t')
-	  newLine=[]
-	  self.names[line[0]]=line[1][:-1]
+	  if len(line)>1:
+	    self.names[line[0]]=line[1][:-1]
 	  line=file.readline()
-	#print "Yes", "René" in self.names
 	file.close()
 	
 	file=open(dicLieux)
@@ -259,7 +225,6 @@ class NameEntities():
 	  line=line.split('\t')
 	  self.places[line[0]]=line[1][:-1]
 	  line=file.readline()
-	#print self.places
 	file.close()   
 	
 	file=open(dicOrgs)
@@ -268,12 +233,10 @@ class NameEntities():
 	  line=line.split('\t')
 	  self.orgs[line[0]]=line[1][:-1]
 	  line=file.readline()
-	#print self.places
 	file.close() 
       
       
 if __name__ == '__main__':
   data=sys.stdin.readlines()
   txt=Texte()
-  #print data
   print txt.detectEN(data)
