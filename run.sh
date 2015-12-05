3df1bf7dc6b3b21b4e2e65f1a241a026299ea869
Arthur Lapraye <arthurlapraye@yahoo.fr>
2015-12-05 01:00:20 +0100

    corrigé un bug dans run.sh + ajout au fichier de test

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ then
     03_spellcheck_compounds/group3.py < out_02.txt > out_03.txt
     python 04_namedEntity/recognitionNE.py < out_03.txt > out_04.txt
 
-    cat 04.out
+    cat out_04.txt
 else
     < in.txt 01_etiq_token_debruit/etape1.py \
     | 02_morpho/etape2.py -p 02_morpho/weights.pickle -l 02_morpho/known.pickle \
